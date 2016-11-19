4d34191bf354f712e1ef77be54c14f8e8e2a1f10
Felix <FFunkGit@lggf.de>
2016-11-19 16:31:25 +0100

    Methode drive verfeinert

--- GitTestProjekt/src/gittest/car/Car.java
+++ GitTestProjekt/src/gittest/car/Car.java
@@ -2,8 +2,9 @@ package gittest.car;
 
 public class Car {
 	
-	private int tank;
+	
 	final private int _verbrauch;
+	private int tank;
 	
 	public Car(int verbrauch){
 		tank = 100;
@@ -11,10 +12,20 @@ public class Car {
 	}
 	
 	public void drive(int km){
-		tank = tank - (_verbrauch*km);
-		if(tank < 10) System.out.println("tank fast leer");
+		int tank_delta = km*_verbrauch;
+		if(tank - tank_delta < 10) {
+			System.out.println("Sie sind " + (tank-10)/_verbrauch + "km gefahren");
+			System.out.println("Das Tankvolumen beträgt 10L, bitte tanken");
+			tank = 10;
+		}
+		else {
+			tank = tank - tank_delta;
+			System.out.println("Sie sind " + km +"km gefahren");
+			System.out.println("Das Tankvolumen beträgt " + tank + "L");
+		}
 	}
 	public void tanken() {
 		tank = 100;
+		System.out.println("Tanken erfolgreich. Das Tankvolumen beträgt " + tank +"L");
 	}
 }
